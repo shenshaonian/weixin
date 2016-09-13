9cf19533e58bd4be76d2db9f68b090c7422ec2eb
shenshaonian <1343513404@qq.com>
2016-09-13 09:19:41 +0800

    初始化项目

---
+++ weixin/weixin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weixin
+//
+//  Created by shenshaonian on 16/9/13.
+//  Copyright (c) 2016年 WuhanFangZhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
